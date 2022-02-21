a9851f04189e8fac347820d3f9beb14517af66be
Dj-777 <dchampavat777@gmail.com>
2022-02-21 15:02:09 +0530

    files are updated

--- router/routerinsertuser.ts
+++ router/routerinsertuser.ts
@@ -4,7 +4,7 @@ import {insertuser,selectuser,updateuser,deleteuser}   from "../usercontroller/u
 const route= Express.Router();
 
     route.post('/insertuser',insertuser,insertuser);
-    route.get('/select',selectuser,selectuser);
-    route.put('/updateuser',updateuser,updateuser);
-    route.delete('/deleteuser',deleteuser,deleteuser);
+    route.get('/select/:id',selectuser,selectuser);
+    route.put('/updateuser/:id',updateuser,updateuser);
+    route.delete('/deleteuser/:id',deleteuser,deleteuser);
 export{route as apps};
--- usercontroller/usercontroller.ts
+++ usercontroller/usercontroller.ts
@@ -1,5 +1,5 @@
 import { Userorm } from "../src/Entities/user";
-import {  getConnection,getRepository } from "typeorm";
+import {  Any, getConnection,getRepository } from "typeorm";
 
 
 export const insertuser= async (req:any,res:any)=>{
@@ -20,36 +20,39 @@ export const insertuser= async (req:any,res:any)=>{
 };
 export const selectuser= async (req:any,res:any)=>{
     //const userormtable=await getRepository(Userorm).createQueryBuilder("userormtable").getMany();
+    
+    const {id}=req.params;
     const useormtable = await getConnection().
-        
+       
     /* You can use getrepository and createquerybuilder(tablename) for avoid of wrting .select("userormtable")
         .from(Userorm,"userormtable") */    
-
         getRepository(Userorm).
         createQueryBuilder('userormtable')
 
         /* .select("userormtable")
         .from(Userorm,"userormtable") */
-        
-        
-        .where("userormtable.id=:id",{id:1}).getOne();
+        .where("userormtable.id=:id",{id:id}).getOne();
         
     console.log(useormtable);
 };
 
 export const updateuser=async(req:any,res:any)=>{
-    const updateuserorm=await getConnection()
-    .createQueryBuilder()
-    .update(Userorm)
-    .set({firstname:"jeel",lastname:"patel",username:'jeel',password:"jeel@123"})
-    .where("id=:id",{id:6})
-    .execute();
+        const {id}=req.params;
+        const {firstname,lastname,username,password,address}=req.body;
+        const updateusers= await getConnection()
+        .createQueryBuilder()
+        .update(Userorm)
+        .set({firstname:firstname,lastname:lastname,username:username,password:password,address:address})
+        .where("id=:id",{id})
+        .execute();
+        return res.json(updateusers);
 
-    console.log(updateuserorm);
 };
 
 export const deleteuser=async(req:any,res:any)=>{
-    const deleteuser=await getConnection().createQueryBuilder().delete().from(Userorm).where("id=:id",{id:4})
-    .execute();
-    console.log(deleteuser);
+    const {id}=req.params;
+    const deleteuser=await Userorm.delete({
+            id,
+    });
+    return res.json(deleteuser);
 };
